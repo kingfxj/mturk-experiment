560f9cfcfa6b129096ec0d636016f97fe3d2784a
walkP <wpeters@ualberta.ca>
2021-02-20 18:16:48 -0700

    Got postgresql working as a backend database with Django. Along with this, I got the polls app from the 'Writing your first Django app' tutorial working (using postgresql). PSQL database information that I used for testing can be found in mturksite/mturksite/settings.py

--- mturksite/mturksite/settings.py
+++ mturksite/mturksite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mturksite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mturksite',
+        'USER': 'postgres',
+        'PASSWORD': 'password1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'MST'
 
 USE_I18N = True
 
