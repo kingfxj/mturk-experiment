75faa8bf1eb0d83c0177260fb508885bc3e008c2
walkP <wpeters@ualberta.ca>
2021-02-25 14:20:57 -0700

    got React integrated with Django so our react app launches on runserver. Configured urls.py to handle React routes. Renamed mturk-app to react-app for clarity.

--- mturksite/mturksite/settings.py
+++ mturksite/mturksite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'mturksite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'react-app/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,8 +82,8 @@ DATABASES = {
         'NAME': 'mturksite',
         'USER': 'mturksite',
         'PASSWORD': 'jd1zIJrLZojgUQmUo83E',
-        # 'HOST': '10.2.9.178',
-        'HOST': '2605:fd00:4:1001:f816:3eff:fe72:95e9',
+        'HOST': '10.2.9.178',
+        #'HOST': '2605:fd00:4:1001:f816:3eff:fe72:95e9',
         'PORT': '5432',
     }
 }
@@ -123,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'react-app/build/static'),
+]
--- mturksite/mturksite/urls.py
+++ mturksite/mturksite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # match the root
+    re_path(r'^$', TemplateView.as_view(template_name='index.html')),
+    # match all other pages
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ]
