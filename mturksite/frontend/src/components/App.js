460f279f7db23af39dede06bc9d2ea6b7a5bf555
Jonathan Ong <jzong@ualberta.ca>
2021-02-25 22:08:56 -0700

    fixed

--- mturksite/frontend/src/components/App.js
+++ mturksite/frontend/src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("")
+    fetch("test/")
       .then((response) => {
         if (response.status > 400) {
           return this.setState(() => {
--- mturksite/mturk_app/urls.py
+++ mturksite/mturk_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.assignmentsListCreate.as_view() ),
+    path('test/', views.assignmentsListCreate.as_view() ),
 ]
