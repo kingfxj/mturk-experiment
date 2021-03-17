716fc761443239ec663642c6af4c65d472be6215
walkP <wpeters@ualberta.ca>
2021-03-17 16:48:53 -0600

    Fixed pages so they all work (waiting for merges)

--- mturksite/mturkapp/models.py
+++ mturksite/mturkapp/models.py
@@ -18,8 +18,8 @@ class Qualification(models.Model):
 class HIT(models.Model):
     hit_id = models.CharField(max_length=100)
     hittype_id = models.CharField(max_length=100)
-    assignments = models.IntegerField()
-    expiry_date = models.CharField(max_length=100)
+    max_assignments = models.IntegerField()
+    expiry_time = models.CharField(max_length=100)
 
     class Meta:
         # Remote database name
@@ -27,7 +27,6 @@ class HIT(models.Model):
 
 
 class HITType(models.Model):
-    batch = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     hittype_id = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
