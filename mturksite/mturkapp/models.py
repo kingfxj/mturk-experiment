3e6dc2af480007cb8ec087a73e1ed3ec42d7f2ac
Muhammad Mazhar Hussain <hussain2@ualberta.ca>
2021-03-17 17:34:35 -0600

    cleaned up code

--- mturksite/mturkapp/models.py
+++ mturksite/mturkapp/models.py
@@ -9,13 +9,8 @@ class Qualification(models.Model):
     int_value = models.IntegerField(blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
     subdivision = models.CharField(max_length=100, blank=True, null=True)
-    # actions_guarded = models.CharField(max_length=100, blank=True, null=True)
     Status = models.BooleanField(default= False)
 
-    class Meta:
-        # Remote database name
-        db_table = "mturk_app_qualifications"
-        
 
 class HIT(models.Model):
     hit_id = models.CharField(max_length=100)
