56d95b3959259fcb781eded4706026baab343871
Jonathan Ong <jzong@ualberta.ca>
2021-04-11 15:30:06 -0600

    code cleanup

--- mturksite/mturkapp/consumers.py
+++ mturksite/mturkapp/consumers.py
@@ -2,18 +2,14 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 import json
 
-
-
 class GameRoom(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['hit_id']
         self.room_group_name = 'room_%s' % self.room_name
-
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
 
     def disconnect(self):
--- mturksite/mturkapp/forms.py
+++ mturksite/mturkapp/forms.py
@@ -4,12 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
 class QualificationForm(forms.ModelForm):
     description = forms.CharField(max_length=2000, required=True)
     country = forms.CharField(max_length=100, required=False)
